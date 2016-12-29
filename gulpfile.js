b754d3541531b59c71407f0478b3d8019d54696a
Aswin S <s.aswin@tcs.com>
2016-12-29 12:06:57 +0530

    Adding prefix min to minified file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp'),
+    rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
 gulp.task('minify', function() {
     gulp.src('src/*.js')
         .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('example'))
+
 });
